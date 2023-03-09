680b1ba456f50e3ef4707044e9ffc72e64813f0a
Dawei He <hdw623@hotmail.com>
2023-03-08 22:43:26 -0500

    black format test

--- test/dptable_test.py
+++ test/dptable_test.py
@@ -3,57 +3,62 @@ import pytest
 from printstructure.printdp import DPTable
 
 
-
 def test_valid_data_type():
-    data = [1,2,3]
+    data = [1, 2, 3]
     dp = DPTable(data)
+
 
 def test_invalid_data_type_tuple():
     with pytest.raises(TypeError):
-        data = tuple([1,2])
+        data = tuple([1, 2])
         dp = DPTable(data)
 
+
 def test_valid_nested_data_type():
-    data = [[1,2,3],[4,5,6]]
+    data = [[1, 2, 3], [4, 5, 6]]
     dp = DPTable(data)
+
 
 def test_invalid_nested_data_type():
     with pytest.raises(TypeError):
-        data = [1, [1,2]]
+        data = [1, [1, 2]]
         dp = DPTable(data)
+
 
 def test_inconsistant_row_lenght():
     with pytest.raises(ValueError):
-        data = [[1], [1,2]]
+        data = [[1], [1, 2]]
         dp = DPTable(data)
 
+
 def test_2d_table_output(capsys):
-    data = data = [['a', 'b', 'c'], ['b', 'c','d'], [1,2,3]]
+    data = data = [["a", "b", "c"], ["b", "c", "d"], [1, 2, 3]]
     dp = DPTable(data)
     dp.print_dp()
     out, err = capsys.readouterr()
     assert out == (
-        '+--------+-----+-----+-----+\n'
-        '|   col# | 0   | 1   | 2   |\n'
-        '|   row# |     |     |     |\n'
-        '+========+=====+=====+=====+\n'
-        '|      0 | a   | b   | c   |\n'
-        '+--------+-----+-----+-----+\n'
-        '|      1 | b   | c   | d   |\n'
-        '+--------+-----+-----+-----+\n'
-        '|      2 | 1   | 2   | 3   |\n'
-        '+--------+-----+-----+-----+\n'
+        "+--------+-----+-----+-----+\n"
+        "|   col# | 0   | 1   | 2   |\n"
+        "|   row# |     |     |     |\n"
+        "+========+=====+=====+=====+\n"
+        "|      0 | a   | b   | c   |\n"
+        "+--------+-----+-----+-----+\n"
+        "|      1 | b   | c   | d   |\n"
+        "+--------+-----+-----+-----+\n"
+        "|      2 | 1   | 2   | 3   |\n"
+        "+--------+-----+-----+-----+\n"
     )
 
+
 def test_1d_table_output(capsys):
-    data = data = [1,2,3]
+    data = data = [1, 2, 3]
     dp = DPTable(data)
     dp.print_dp()
     out, err = capsys.readouterr()
     assert out == (
-         '+--------+-----+-----+-----+\n'
-         '| col#   |   0 |   1 |   2 |\n'
-         '+========+=====+=====+=====+\n'
-         '|        |   1 |   2 |   3 |\n'
-         '+--------+-----+-----+-----+\n'
+        "+--------+-----+-----+-----+\n"
+        "| col#   |   0 |   1 |   2 |\n"
+        "+========+=====+=====+=====+\n"
+        "|        |   1 |   2 |   3 |\n"
+        "+--------+-----+-----+-----+\n"
     )
